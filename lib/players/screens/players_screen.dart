a02dbf04f4b862a9edc558b6306308180fabe775
Sebastian Lisiecki <lislav.hms@gmail.com>
2022-10-21 17:06:51 +0200

    Redesign players screen

--- lib/players/screens/players_screen.dart
+++ lib/players/screens/players_screen.dart
@@ -12,92 +12,129 @@ class PlayersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Gracze')),
+      appBar: AppBar(
+        title: const Text('Gracze'),
+      ),
       body: SafeArea(
-        child: Column(
+        child: Stack(
+          clipBehavior: Clip.none,
           children: [
-            Expanded(
-              child: BlocBuilder<PlayersCubit, PlayersState>(
-                builder: (context, state) {
-                  final players = state.players;
-                  if (players.isEmpty) {
-                    return const Center(
-                      child: Text('Brak graczy'),
-                    );
-                  }
-
-                  return ListView(
-                    shrinkWrap: true,
-                    children: players
-                        .map(
-                          (item) => ListTile(
-                            leading: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('${players.indexOf(item) + 1}.'),
-                              ],
-                            ),
-                            title: Text(item),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  icon: const Icon(Icons.edit),
-                                  onPressed: () {
-                                    Navigator.of(context)
-                                        .push(MaterialPageRoute(
-                                      builder: (context) => BlocProvider.value(
-                                        value: getIt.get<PlayersCubit>(),
-                                        child:
-                                            AddEditPlayerScreen(player: item),
-                                      ),
-                                    ));
-                                  },
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.close),
-                                  onPressed: () async {
-                                    final cubit = context.read<PlayersCubit>();
-                                    final doRemove = await showDecisionDialog(
-                                          context,
-                                          title:
-                                              'Czy chcesz usunąć gracza $item?',
-                                        ) ??
-                                        false;
-
-                                    if (doRemove) {
-                                      cubit.removePlayer(item);
-                                    }
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                        .toList(),
+            BlocBuilder<PlayersCubit, PlayersState>(
+              builder: (context, state) {
+                final players = state.players;
+                if (players.isEmpty) {
+                  return const Center(
+                    child: Text('Brak graczy'),
                   );
-                },
-              ),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => BlocProvider.value(
-                        value: getIt.get<PlayersCubit>(),
-                        child: const AddEditPlayerScreen(),
+                }
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 76.0),
+                  child: Scrollbar(
+                    thumbVisibility: true,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: ListView(
+                        children: _generateListChildren(
+                          context,
+                          players: players,
+                        ),
                       ),
-                    ));
-                  },
-                  child: const Text('Dodaj gracza'),
-                ),
-              ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            Positioned(
+              right: 16,
+              bottom: 20,
+              child: FloatingActionButton(
+                onPressed: () => _onAddEditPlayer(context),
+                child: const Icon(Icons.add),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  List<Widget> _generateListChildren(
+    BuildContext context, {
+    required List<String> players,
+  }) {
+    Widget listItemBuilder(int index, String player) {
+      return Expanded(
+        child: Card(
+          child: ListTile(
+            minLeadingWidth: 20.0,
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('${index + 1}.')],
+            ),
+            title: FittedBox(
+              alignment: Alignment.centerLeft,
+              fit: BoxFit.scaleDown,
+              child: Text(player),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () => _onAddEditPlayer(context, player: player),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () => _onPlayerDelete(context, player),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    final children = <Widget>[];
+    for (var i = 0; i < players.length; i += 2) {
+      final player1 = players[i];
+      final player2 = i + 1 >= players.length ? null : players[i + 1];
+
+      final row = Row(
+        children: [
+          listItemBuilder(i, player1),
+          if (player2 != null) listItemBuilder(i + 1, player2),
+        ],
+      );
+
+      children.add(row);
+    }
+
+    return children;
+  }
+
+  void _onAddEditPlayer(BuildContext context, {String? player}) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => BlocProvider.value(
+          value: getIt.get<PlayersCubit>(),
+          child: AddEditPlayerScreen(player: player),
+        ),
+      ),
+    );
+  }
+
+  void _onPlayerDelete(BuildContext context, String player) async {
+    final cubit = context.read<PlayersCubit>();
+    final doRemove = await showDecisionDialog(
+          context,
+          title: 'Czy chcesz usunąć gracza $player?',
+        ) ??
+        false;
+
+    if (doRemove) {
+      cubit.removePlayer(player);
+    }
   }
 }
